574fc7b9d3e6054bbedf82fec3e8031950928a0e
Lynda Johns <lyndajoncaf@gmail.com>
2023-08-09 16:08:19 -0400

    built contact form

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -10,7 +10,40 @@ export const ContactForm = ({
   handleSubmit
 }) => {
   return (
-    <></>
+    <form onSubmit={handleSubmit}>
+      <label>
+        Name:
+        <input
+          type="text"
+          value={name}      
+          onChange={(e) => setName(e.target.value)}
+          required
+        />
+      </label>
+      <br />
+      <label>
+        Phone:
+        <input
+          type="text"
+          value={phone}      
+          onChange={(e) => setPhone(e.target.value)}
+          pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+          required
+        />
+      </label>
+      <br />
+      <label>
+        Email:
+        <input
+          type="text"
+          value={email}      
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+      </label>
+      <br />
+      <button>Add Contact</button>
+    </form>
   );
 };
 
