7dd6ae3298d057bb41e167df6625412b6efbd2c1
Lynda Johns <lyndajoncaf@gmail.com>
2023-08-09 13:41:14 -0400

    completed contacts page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route, Navigate } from "react-router-dom"
 import Root, { ROUTES } from "./components/root/Root";
 import { AppointmentsPage } from "./containers/appointmentsPage/AppointmentsPage";
@@ -9,17 +9,24 @@ function App() {
   Define state variables for 
   contacts and appointments 
   */
-
+  const [contacts, setContacts] = useState([]);
+  const [appointments, setAppointments] = useState([]);
   /*
   Implement functions to add data to
   contacts and appointments
   */
+  const addContact = (newContact) => {
+    setContacts((prevContacts) => [...prevContacts, newContact]);
+  };
+  const addAppointment = (newAppointment) => {
+    setAppointments((prevAppointments) => [...prevAppointments, newAppointment]);
+  };
 
   const router = createBrowserRouter(createRoutesFromElements(
     <Route path="/" element={ <Root/> }>
       <Route index element={ <Navigate to={ROUTES.CONTACTS} replace/> }/>
-      <Route path={ROUTES.CONTACTS} element={ <ContactsPage /> /* Add props to ContactsPage */ }/>
-      <Route path={ROUTES.APPOINTMENTS} element={ <AppointmentsPage /> /* Add props to AppointmentsPage */ }/>
+      <Route path={ROUTES.CONTACTS} element={ <ContactsPage contacts={contacts} addContact={addContact} /> /* Add props to ContactsPage */ }/>
+      <Route path={ROUTES.APPOINTMENTS} element={ <AppointmentsPage appointments={appointments} contacts={contacts} addAppointment={addAppointment}  /> /* Add props to AppointmentsPage */ }/>
     </Route>
   ));
   
--- src/containers/contactsPage/ContactsPage.js
+++ src/containers/contactsPage/ContactsPage.js
@@ -3,11 +3,15 @@ import React, { useState, useEffect } from "react";
 import { ContactForm } from "../../components/contactForm/ContactForm";
 import { TileList } from "../../components/tileList/TileList";
 
-export const ContactsPage = () => {
+export const ContactsPage = ({contacts, addContact}) => {
   /*
   Define state variables for 
   contact info and duplicate check
   */
+ const [name, setName] = useState("");
+ const [phone, setPhone] = useState("");
+ const [email, setEmail] = useState("");
+ const [isDuplicate, setIsDuplicate] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -15,6 +19,15 @@ export const ContactsPage = () => {
     Add contact info and clear data
     if the contact name is not a duplicate
     */
+   if (isDuplicate) {
+      return;
+   } 
+    addContact({name, phone, email});
+    setName("");
+    setPhone("");
+    setEmail("");
+    setIsDuplicate(false);
+   
   };
 
   /*
@@ -26,10 +39,22 @@ export const ContactsPage = () => {
     <div>
       <section>
         <h2>Add Contact</h2> 
+        <ContactForm
+          name={name}
+          setName={setName}
+          phone={phone}
+          setPhone={setPhone}
+          email={setEmail}
+          isDuplicate={isDuplicate}
+          handleSubmit={handleSubmit}
+          />
       </section>
       <hr />
       <section>
         <h2>Contacts</h2>
+        <TileList
+          contacts={contacts}
+        />
       </section>
     </div>
   );
