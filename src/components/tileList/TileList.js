dc1cb1b026e13de4e32d6e4791a4f5d1b40013b1
Lynda Johns <lyndajoncaf@gmail.com>
2023-08-10 11:44:52 -0400

    completed TileList component

--- src/components/tileList/TileList.js
+++ src/components/tileList/TileList.js
@@ -1,9 +1,20 @@
 import React from "react";
+import { Tile } from "../tile/Tile";
 
-export const TileList = () => {
+
+export const TileList = ({ data }) => {
   return (
     <div>
-      
+      {data.map(({ name, ...rest }, index) => (
+        <Tile
+          key={index}
+          name={name}
+          {...rest}
+        />
+      ))}
     </div>
   );
+};
+TileList.defaultProps = {
+  data: [], // Provide a default empty array if data is not provided
 };
